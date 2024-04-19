c53ac033fd9d16d4bf1e9c064d801f1324e880ad
Omaury Marmolejo <omaury@tango.io>
2024-04-19 10:08:14 -0600

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,22 +6,22 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
-  map_public_ip_on_launch = true
-  availability_zone       = "us-west-2a"
-}
+# resource "aws_subnet" "public" {
+#   vpc_id                  = var.vpc_id
+#   cidr_block              = "10.0.1.0/24"
+#   map_public_ip_on_launch = true
+#   availability_zone       = "us-west-2a"
+# }
 
-resource "aws_subnet" "subnet2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
+resource "aws_subnet" "private" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-west-2b"
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = var.vpc_id
 
   tags = {
     Name = "internet_gateway"
@@ -29,7 +29,7 @@ resource "aws_internet_gateway" "internet_gateway" {
 }
 
 resource "aws_route_table" "route_table" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = var.vpc_id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -38,18 +38,18 @@ resource "aws_route_table" "route_table" {
 }
 
 resource "aws_route_table_association" "subnet_route" {
-  subnet_id      = aws_subnet.subnet.id
+  subnet_id      = var.public_subnet_id
   route_table_id = aws_route_table.route_table.id
 }
 
 resource "aws_route_table_association" "subnet2_route" {
-  subnet_id      = aws_subnet.subnet2.id
+  subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.route_table.id
 }
 
 resource "aws_security_group" "security_group" {
-  name   = "esc-security-group"
-  vpc_id = aws_vpc.main.id
+  name   = "railswave-security-group"
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 0
